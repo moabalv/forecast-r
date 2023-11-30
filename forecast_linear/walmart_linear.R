5d576e265952e71f204bdf31ef71fd7e360d99f1
moabalv <moab.alves.silva@ccc.ufcg.edu.br>
2023-11-30 14:17:01 -0300

    Atualiza walmart_linear.R

--- forecast_linear/walmart_linear.R
+++ forecast_linear/walmart_linear.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(forecast)
 
-
 data <- read_csv("https://raw.githubusercontent.com/moabalv/analise-dados/main/walmart-sales-dataset-of-45stores.csv")
 names(data)
 lojas <- data %>%
@@ -17,8 +16,9 @@ ts_loja <- loja_1 %>%
   select(weekly_sales) %>%
   ts(frequency = 52, start = c(2010,5), end = c(2012,43))
 
-ggplot(ts_loja) +
-  geom_line(mapping = aes(y = ts_loja[], x = loja_1$date)) +
+dff <- tibble(data.frame(timestamp = loja_1$date, values = ts_loja))
+ggplot(dff, aes(timestamp, weekly_sales)) +
+  geom_line() +
   scale_x_datetime(name = "Ano e mês", date_labels = "%Y-%m", date_breaks = "3 month") + 
   labs(y = "Vendas") +
   theme_bw()
@@ -36,6 +36,31 @@ accuracy(fc, ts_loja.test)
 
 fit_loja1 <- tslm(ts_loja.train ~ season + trend)
 fc_linear <- forecast(fit_loja1, h = 28)
+fc_linear
+
+media <- fc_linear$mean
+upper <- fc_linear$upper
+lower <- fc_linear$lower
+train <- tail(ts_loja.train, 10)
+
+teste <- zoo::as.yearmon(time(media))
+
+teste
+### meu autoplot ####
+
+ggplot(data = media, aes(y = media[], x = time(media))) +
+  geom_line(color = "blue")+
+  geom_ribbon(aes(ymin = lower[,1], ymax = upper[,1]), alpha = 0.35, fill = "blue") +
+  geom_ribbon(aes(ymin = lower[,2], ymax = upper[,2]), alpha = 0.15, fill = "blue") +
+  labs(title = "Comparação vendas reais e previstas", y = "Vendas", x = "Tempo") +
+  geom_line(train, mapping = aes(x = time(train), y = train[])) +
+  scale_y_continuous() +
+  geom_line(ts_loja.test, mapping = aes(x= time(ts_loja.test), y = ts_loja.test[]), color =  "red")
+
+#######################
+
+autoplot(fc_linear, include = 10) +
+  geom_line(ts_loja.test, mapping = aes(x= time(ts_loja.test), y = ts_loja.test[]), color =  "red")
 
 autoplot(fc_linear, include = 10, series = "Previsto") +
   autolayer(ts_loja.test, series = "Observado") +
@@ -44,3 +69,5 @@ autoplot(fc_linear, include = 10, series = "Previsto") +
 
 accuracy(fc_linear, ts_loja.test)
 autoplot(checkresiduals(fit_loja1))
+
+###
